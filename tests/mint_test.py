9bd4d3623db6e8cf3ee188664d3d7d1848005c43
lucadonnoh <donnoh99@gmail.com>
2021-01-02 15:01:32 +0100

    check new balance after mint

--- tests/mint_test.py
+++ tests/mint_test.py
@@ -23,15 +23,23 @@ def test_mint(contracts, exValue, web3):
 
     # send wBTC to minter and check balance
     wBTC.mint(minter, 10**9, {'from': satoshi})
-    assert wBTC.balanceOf(minter) == 10**9
+    balance = wBTC.balanceOf(minter)
+    print("minter balance    : ", balance)
+    assert balance == 10**9 
 
     # approve wBTC to PetRockNFT to spend on behalf of minter and check that it worked
     wBTC.approve(prf.address, exValue, {'from': str(minter)})
     allowance = wBTC.allowance(minter.address, web3.toChecksumAddress(prf.address))
+    print("minter allowance  : ", allowance)
     assert allowance == exValue
 
     # minter mints petrock 
     prf.mintNewPetRock(minter, "Fabio", 10**8, {'from': str(minter)})
+
+    # check minter new balance
+    newBalance = wBTC.balanceOf(minter)
+    print("minter new balance: ", newBalance)
+    assert newBalance == balance-10**8
 
     # check mint worked
     assert prf.totalSupply() == 1
