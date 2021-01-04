4d9de7263186be0652980ab5919768dfac5d80a4
lucadonnoh <donnoh99@gmail.com>
2021-01-04 12:40:10 +0100

    insufficient balance test

--- tests/mint_test.py
+++ tests/mint_test.py
@@ -1,5 +1,7 @@
 import pytest
+import brownie
 from brownie import PetRockNFT, WBTC, accounts
+from brownie.test import given, strategy
 
 @pytest.fixture
 def exValue():
@@ -11,7 +13,8 @@ def contracts():
     prf = PetRockNFT.deploy(wbtc, {'from': accounts[1]})
     return [prf, wbtc]
 
-def test_mint(contracts, exValue, web3):
+@given(numTokens=strategy('uint256', min_value=10**8, max_value=21*10**6*10**8))
+def test_mint(contracts, exValue, numTokens, web3):
     # contracts
     prf = contracts[0]
     wBTC = contracts[1]
@@ -22,10 +25,10 @@ def test_mint(contracts, exValue, web3):
     god = accounts[1]
 
     # send wBTC to minter and check balance
-    wBTC.mint(minter, 10**9, {'from': satoshi})
+    wBTC.mint(minter, numTokens, {'from': satoshi})
     balance = wBTC.balanceOf(minter)
     print("minter balance    : ", balance)
-    assert balance == 10**9 
+    assert balance == numTokens
 
     # approve wBTC to PetRockNFT to spend on behalf of minter and check that it worked
     wBTC.approve(prf.address, exValue, {'from': str(minter)})
@@ -45,3 +48,27 @@ def test_mint(contracts, exValue, web3):
     assert prf.totalSupply() == 1
     assert prf.balanceOf(minter) == 1
     assert str(prf.ownerOf(0)) == minter.address
+
+@given(numTokens=strategy('uint256', min_value=0, max_value=10**8-1))
+def test_mint_insufficientBalance(contracts, numTokens, exValue, web3):
+    # contracts
+    prf = contracts[0]
+    wBTC = contracts[1]
+
+    # accounts
+    satoshi = accounts[5]
+    minter = accounts[0]
+    god = accounts[1]
+
+    # send wBTC to minter and check balance
+    wBTC.mint(minter, numTokens, {'from': satoshi})
+    balance = wBTC.balanceOf(minter)
+    print("minter balance    : ", balance)
+    assert balance == numTokens
+    wBTC.approve(prf.address, numTokens, {'from': str(minter)})
+
+    with brownie.reverts("You need to send 1 wBTC"):
+        prf.mintNewPetRock(minter, "Reverto", 10**8)
+    
+    assert prf.totalSupply() == 0
+
